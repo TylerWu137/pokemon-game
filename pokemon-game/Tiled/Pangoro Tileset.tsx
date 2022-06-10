a77feb4d89efc0e0e2a9738d8249c6c7c448fe9c
LittleDragon137 <75277804+LittleDragon137@users.noreply.github.com>
2022-06-10 09:55:11 -0400

    Add files via upload

---
+++ pokemon-game/Tiled/Pangoro Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Pangoro Tileset" tilewidth="12" tileheight="12" tilecount="441" columns="21">
+ <image source="Pangoro Set.png" width="256" height="256"/>
+</tileset>
